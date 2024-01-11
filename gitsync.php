352bcf045c5ebb8b801d55a0fba3478a3e6d7a2c
Nirbhay Singh Rao <nirbhay@live.in>
2024-01-11 22:06:46 +0530

    Init WordPress Plugin

---
+++ gitsync.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Gitsync
+   Plugin URI: #
+   description: Sync plugins from Github
+   Version: 0.0.1
+   Author: Nirbhay Singh Rao
+   Author URI: #
+   License: GPL3
+   */
+?>
